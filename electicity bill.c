925005d153d94a66f6896be840ba1a58c695187b
Pavankumarkaki <96739003+Pavankumarkaki@users.noreply.github.com>
2021-12-28 15:45:16 +0530

    Add files via upload

--- electicity bill.c
+++ electicity bill.c
@@ -1,46 +1,30 @@
 #include<stdio.h>
 void main()
 {
-	float units,bill;
+	float units,bill,sur=0.00;
 	printf("enter the consumed units : ");
 	scanf("%f",&units);
 	if ( units<=199)
 	{ 
 	bill=units*1.20;
-	printf("the bill is %.2f",bill);
-	if(bill>400)
-	{
-		printf(" \nthe surcharge is %.2f",bill*15/100);
-		printf("\n the total bill is %.2f",bill+(bill*15/100));
-	}
 	}
 	else if ( units>=200 && units<400)
 	{
 		bill=units*1.50;
-		printf("the bill is %.2f",bill);
-		if(bill>400)
-	{
-		printf(" \nthe surcharge is %.2f",bill*15/100);
-		printf("\nthe total bill is %.2f",bill+(bill*15/100));
-	}
 	}
 	else if (units>=400 && units<600)
 	{
 		bill=units*1.80;
-		printf("the bill is %.2f",bill);
-		if(bill>400)
-	{
-		printf(" \nthe surcharge is %.2f",bill*15/100);
-		printf("\nthe total bill is %.2f",bill+(bill*15/100));
-	}
 	}
 	else
 	{
-		bill=units*2;
-		printf("the bill is %.2f",bill);if(bill>400)
+		bill=units*2.00;
+	}
+	if( bill>400)
 	{
-		printf(" \nthe surcharge is %.2f",bill*15/100);
-		printf("\nthe total bill is %.2f",bill+(bill*15/100));
+		sur=bill*0.15;
 	}
-	}
+	printf("the bill is %.2f",bill);
+	printf("\nthe surcharge is %.2f",sur);
+	printf("\nthe total net bill is %.2f",bill+sur);
 }
